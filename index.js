a83681c568e06dfc6988df63efcba2a3f8a9c674
Khushbu7i <96694658+Khushbu7i@users.noreply.github.com>
2022-01-24 16:11:12 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+function add() {
+
+    var a = document.getElementById('num1').value;
+    var b = document.getElementById('num2').value;
+    document.getElementById('answer').innerHTML=(a+b);
+}   
