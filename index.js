4eb30add524124de6e74dcb3d5210e77bda75845
Khushbu7i <96694658+Khushbu7i@users.noreply.github.com>
2022-01-25 09:49:40 +0530

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-function add() {
-
-    var a = document.getElementById('num1').value;
-    var b = document.getElementById('num2').value;
-    document.getElementById('answer').innerHTML=(a+b);
-}   
